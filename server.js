431bc1b152c866bb3b99ded11e90993424ff60db
ngaeninurul <nurulngaeni13@student.uns.ac.id>
2023-01-05 14:11:35 +0700

    Docker Workflows

--- server.js
+++ server.js
@@ -7,41 +7,41 @@
  */
 
 /* Required Module */
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 
 /* ============ */
 const PORT = process.env.PORT || 3000;
-const general = require('./src/routes/general.routes');
-const movies = require('./src/routes/movie.routes');
-const series = require('./src/routes/tv_series.routes');
-const genres = require('./src/routes/genre.routes');
+const general = require("./src/routes/general.routes");
+const movies = require("./src/routes/movie.routes");
+const series = require("./src/routes/tv_series.routes");
+const genres = require("./src/routes/genre.routes");
 const app = express();
 
 /* app */
 app.use(cors());
 app.use(helmet());
-app.use('/api', general);
-app.use('/api/movie', movies);
-app.use('/api/series', series);
-app.use('/api/genre', genres);
-app.use(express.static('./public'));
+app.use("/api", general);
+app.use("/api/movie", movies);
+app.use("/api/series", series);
+app.use("/api/genre", genres);
+app.use(express.static("./public"));
 
 /* Status */
-app.use('/api', async (req, res) => {
-    res.send({
-        status: true,
-        message: 'IDLIX Scrapper',
-        repo: 'annurdien',
-    });
+app.use("/api", async (req, res) => {
+  res.send({
+    status: true,
+    message: "IDLIX Scrapper",
+    repo: "annurdien",
+  });
 });
 
-app.use('*', async (req, res) => {
-    res.status(404).send({ status: false, message: 'api not found' });
+app.use("*", async (req, res) => {
+  res.status(404).send({ status: false, message: "api not found" });
 });
 
 /* Listener */
 app.listen(PORT, async () => {
-    console.log('Listening on PORT ' + PORT);
+  console.log("Listening on PORT " + PORT);
 });
